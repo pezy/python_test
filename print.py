387589b2db20e7876afc4ba3b66b99f4f443056c
pezy_pc <urbancpz@gmail.com>
2014-03-25 19:11:50 +0800

    add print.py
    Signed-off-by: pezy_pc <urbancpz@gmail.com>

---
+++ print.py
@@ -0,0 +1,14 @@
+# -- coding: utf-8 --
+
+print "Hello, World!"
+print "Hello Again"
+print "I like typing this"
+print "This is fun"
+print 'Yet! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print "ÖÐÎÄ£¡"
+
+# this is comment
+# you can print one line by ','
+print "test just one line,","also one line,","yes good!"
